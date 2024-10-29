6e2799506e72d27e58e248ed49d73cbd74a28bd7
tami yosef <tamiyosef97@gmail.com>
2024-10-29 21:43:04 +0200

    //

--- ViewModels/LoginPageViewModel.cs
+++ ViewModels/LoginPageViewModel.cs
@@ -20,9 +20,36 @@ public class LoginPageViewModel : ViewModelBase
 
     public ICommand LoginCommand { get; set; }
     public ICommand GoToRegisterCommand { get; set; }
-    public string Email { get; set; }
-
-    public string Password { get; set; }
+    private string email;
+    public string Email
+    {
+        get => email;
+        set
+        {
+            if (email != value)
+            {
+                email = value;
+                OnPropertyChanged(nameof(Email));
+                // can add more logic here like email validation etc.
+                // can add error message property and set it here
+            }
+        }
+    }
+    private string password;
+    public string Password
+    {
+        get => password;
+        set
+        {
+            if (password != value)
+            {
+                password = value;
+                OnPropertyChanged(nameof(Password));
+                // can add more logic here like email validation etc.
+                // can add error message property and set it here
+            }
+        }
+    }
 
     public LoginPageViewModel(SweetBoxWebApi service, IServiceProvider serviceProvider)
 	{
